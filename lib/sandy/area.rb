c661230a085aadf6b468794d0bc20f56b867acbc
Cameron Cundiff <ckundo@gmail.com>
2012-11-02 15:08:30 -0400

    Adding area attributes

--- lib/sandy/area.rb
+++ lib/sandy/area.rb
@@ -1,4 +1,14 @@
 module Sandy
   class Area
+    attr_reader :name, :region, :customers_affected, :latitude, :longitude
+
+    def initialize(customers_affected, location, options = {})
+      @name = location
+      @customers_affected = customers_affected
+      @region = options[:region]
+      @total_customers = options[:total_customers]
+      @latitude = options[:latitude]
+      @longitude = options[:longitude]
+    end
   end
 end
---
+++ spec/area_spec.rb
@@ -0,0 +1,33 @@
+require "spec_helper"
+
+describe Sandy::Area do
+  let(:customers_affected) { 100 }
+  let(:options) { {} }
+
+  describe "#customers_affected" do
+    subject { Sandy::Area.new(customers_affected, nil).customers_affected }
+    it { should == customers_affected }
+  end
+
+  context "with an area name" do
+    let(:area_name) { "Battery Park" }
+    describe "#name" do
+      subject { Sandy::Area.new(customers_affected, area_name).name }
+      it { should == area_name }
+    end
+  end
+
+  context "with a region name" do
+    describe "#parent_region" do
+      subject { Sandy::Area.new(customers_affected, nil, { region: "Manhattan" }).region }
+      it { should == "Manhattan" }
+    end
+  end
+
+  context "with lat/lng" do
+    subject { Sandy::Area.new(customers_affected, nil, { latitude: 70.5, longitude: -115.5 }) }
+    its(:latitude) { should == 70.5 }
+    its(:longitude) { should == -115.5 }
+  end
+end
+
