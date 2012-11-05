7f262c31b91fc4e0d1c978361c255f3990361041
Jonthan Pettersson <jonathan.anders.pettersson@gmail.com>
2012-11-05 02:50:21 -0500

    Refactored the ConEd and LIPA Report classes to use the common StormCenter utility class. The slightly different XML tree structures between the providers is handled by a lambda in the second argument passed to the recursive_fetch_areas method. The function is selects how to query the tree for the next level.

--- lib/sandy/providers/coned/report.rb
+++ lib/sandy/providers/coned/report.rb
@@ -1,44 +1,23 @@
 module Sandy::Provider
   module ConEd
     class Report
-      attr_reader :regions, :neighborhoods
+      include Sandy::Provider::StormCenter
+      attr_reader :areas
 
       def initialize
         raw_report = JSON.parse(HTTParty.get(coned_url))
         area_hash = raw_report.fetch("file_data")["curr_custs_aff"]["areas"].first["areas"]
-        @regions = regions_from_report(area_hash)
-        @neighborhoods = neighborhoods_from_report(area_hash)
+        
+        @areas = recursive_fetch_areas(
+          area_hash, 
+          ->(area) { area.fetch("areas", nil) } 
+        )
+
       rescue
         raise LoadError, "ConEd reponse was not recognizable."
       end
 
       private
-
-      def regions_from_report(areas)
-        areas.collect do |area|
-          Sandy::Area.new(area.fetch("custs_out"), area.fetch("area_name"),
-                          { latitude: area["latitude"], 
-                            longitude: area["longitude"],
-                            estimated_recovery_time: area["etr"],
-                            total_customers: area["total_custs"] })
-
-        end
-      end
-
-      def neighborhoods_from_report(areas)
-        neighborhoods = []
-        areas.each do |area|
-          area["areas"].each do |sub_area|
-            neighborhoods << Sandy::Area.new(sub_area.fetch("custs_out"), sub_area.fetch("area_name"),
-                            { region: area["area_name"], 
-                              total_customers: sub_area["total_custs"],
-                              estimated_recovery_time: sub_area["etr"],
-                              latitude: sub_area["latitude"], 
-                              longitude: sub_area["longitude"] })
-          end
-        end
-        neighborhoods
-      end
 
       def coned_url
         base_uri = "http://apps.coned.com"
@@ -47,6 +26,7 @@ module Sandy::Provider
         directory = response.parsed_response.fetch("root").fetch("directory")
         "#{base_uri}/stormcenter_external/stormcenter_externaldata/data/interval_generation_data/#{directory}/report.js"
       end
+
     end
   end
 end
--- lib/sandy/providers/lipa/report.rb
+++ lib/sandy/providers/lipa/report.rb
@@ -1,15 +1,20 @@
 module Sandy::Provider
   module LIPA
     class Report
-      attr_reader :regions, :neighborhoods
+      include Sandy::Provider::StormCenter
+      attr_reader :areas
 
       def initialize
         raw_report = HTTParty.get(lipa_url, format: :xml)
         area_hash = raw_report.fetch("root").fetch("curr_custs_aff")["areas"]["area"]["areas"].first[1]
-        @regions = regions_from_report(area_hash)
-        @neighborhoods = neighborhoods_from_report(area_hash)
+        
+        @areas = recursive_fetch_areas(
+          area_hash, 
+          ->(area) { area.fetch("areas", {}).fetch("area", nil) } 
+        )
+
       rescue
-        raise LoadError, "ConEd reponse was not recognizable."
+        raise LoadError, "LIPA reponse was not recognizable."
       end
       
       private
@@ -25,40 +30,6 @@ module Sandy::Provider
         "#{base_uri}/data/interval_generation_data/#{directory}/data.xml?timestamp=#{time}"
       end
 
-      def regions_from_report(areas)
-        areas.collect do |area|
-          Sandy::Area.new(area.fetch("custs_out"), area.fetch("area_name"),
-                          { estimated_recovery_time: area["etr"],
-                            total_customers: area["total_custs"] })
-
-        end
-      end
-
-      def neighborhoods_from_report(areas_hash)
-        neighborhoods = []
-        areas_hash.each do |region|
-          region["areas"].each do |sub_region|
-            sub_sub_regions = sub_region[1]
-            sub_sub_regions.each do |sub_sub_region|
-              begin
-                hoods = sub_sub_region["areas"]["area"]
-              rescue
-                # FIXME: raise warning if any region is empty
-                next
-              end
-              hoods.each do |hood|
-                total = hood.fetch("total_custs").to_i
-                affected = hood.fetch("custs_out").to_i
-                name = hood.fetch("area_name")
-                region = sub_sub_region.fetch("area_name")
-
-                neighborhoods << Sandy::Area.new(affected, name, { region: region, total_customers: total})
-              end
-            end
-          end
-        end
-        neighborhoods
-      end
     end
   end
 end
